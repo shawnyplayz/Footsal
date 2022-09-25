692202ab64222aca72a08f22f623ae8153b0b38b
Shawn Almeida <55237616+shawnyplayz@users.noreply.github.com>
2022-09-25 13:38:37 +0530

    Created the search bar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,92 +12,82 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animeArr: [],
-      animeLength: null,
+      footArr: [],
+      footLength: null,
       searchQuery: "",
+      searchArr: [],
       page: 1,
+      searchTrigger: false,
     };
     this.handleAll = this.handleAll.bind(this);
-    this.clickNextButton = this.clickNextButton.bind(this);
-    this.clickPrevButton = this.clickPrevButton.bind(this);
   }
   componentDidMount() {
     this.firstCall(this.state.page);
   }
   firstCall = async (page) => {
-    debugger;
     let asd = [];
+    let revasd = [];
     await axios
-      .get(
-        `https://api.jikan.moe/v4/characters?page=${page}&limit=15&q&order_by=fav
-orites&sort=desc`
-      )
+      .get(`https://api.npoint.io/20c1afef1661881ddc9c`)
       // .get(`https://hacker-news.firebaseio.com/v0/item/${i + 1}.json`)
       .then(function (response) {
         asd.push(response.data);
+        revasd = asd[0].playerList.reverse();
+        console.log("rev==>", revasd);
       })
       .catch(function (error) {
         console.log(error);
       });
-    debugger;
     this.setState({
-      animeArr: asd[0].data,
-      animeLength: asd[0].pagination.items.total,
+      footArr: revasd,
+      footLength: asd[0].playerList.length,
     });
+    console.log(this.state.footArr);
   };
-  async search(page) {
+  search() {
+    let search = this.state.searchQuery.toLowerCase();
+    let searchLength = search.length;
+    const result = this.intenseSearch(search);
+
+    this.setState({
+      searchArr: result,
+    });
+  }
+  intenseSearch(searched) {
     debugger;
     let asd = [];
-    if (this.state.searchQuery === "") {
-      this.firstCall(page);
-    } else {
-      let urlSearch = this.state.searchQuery;
-      console.log(urlSearch);
-      await axios
-        .get(
-          `https://api.jikan.moe/v4/characters?page=${page}&limit=15&q=${urlSearch}&order_by=fav
-orites&sort=desc`
-        )
-        .then(function (response) {
-          asd.push(response.data);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-      // if ((asd[0].data).length === 0) {
-      //   this.setState({
-      //     animeArr: null
-      //   })
-      // }
-      this.setState({
-        animeArr: asd[0].data ? asd[0].data : [],
-        animeLength: asd[0].pagination.items.total,
-      });
+    let searchLength = searched.length;
+    for (let i = 0; i < this.state.footLength; i++) {
+      debugger;
+      if (
+        searched ===
+          this.state.footArr[i].PFName.substring(
+            0,
+            searchLength
+          ).toLowerCase() ||
+        searched ===
+          this.state.footArr[i].TName.substring(0, searchLength).toLowerCase()
+      ) {
+        asd.push(this.state.footArr[i]);
+      }
     }
+    return asd;
   }
-
   async handleAll(e) {
-    debugger;
     await this.setState({
       searchQuery: e.target.value,
-      page: 1,
     });
+    if (this.state.searchQuery !== "")
+      this.setState({
+        searchTrigger: true,
+      });
+    else
+      this.setState({
+        searchTrigger: false,
+      });
     setTimeout(() => {
-      this.search(this.state.page);
+      this.search();
     }, 1000);
-  }
-  async clickNextButton() {
-    debugger;
-    await this.setState({
-      page: this.state.page + 1,
-    });
-    this.search(this.state.page);
-  }
-  async clickPrevButton() {
-    await this.setState({
-      page: this.state.page - 1,
-    });
-    this.search(this.state.page);
   }
   render() {
     return (
@@ -110,7 +100,7 @@ orites&sort=desc`
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="Search your Character"
+                  placeholder="Search your Favourite Footballer or Team"
                   onChange={this.handleAll}
                 />
                 <div className="input-group-append">
@@ -126,96 +116,122 @@ orites&sort=desc`
             </div>
           </div>
         </div>
-        {/* <div className="row justify-content-center ">
-          <div className="col-lg-6 col-md-6 col-sm-12">
-            <h5>
-              Total {this.state.animeLength} matching anime Characters found
-            </h5>
-          </div>
-        </div> */}
-        <div className="row  shadow-lg">
+        <div className="row shadow-lg">
           <div className="row justify-content-center">
             <div className="col-lg-12 opaque">
               <div className=" p-2 mt-4">
-                {this.state.animeLength !== 0 ? (
-                  this.state.animeArr.map((el, index) => {
-                    return (
-                      <>
-                        <div className="card my-2 shadow " key={index}>
-                          <div className="row ">
-                            <div className="col-1">
-                              <div className="wrapper-img">
-                                <img
-                                  src={el.images.jpg.image_url}
-                                  alt="img"
-                                  className="img-fluid  anime-img"
-                                />
+                {this.state.searchTrigger === false
+                  ? this.state.footArr.map((el, index) => {
+                      const Images = React.lazy(() =>
+                        import(`../../player-images/${el.Id}`)
+                      );
+                      return (
+                        <>
+                          <div className="card my-2 shadow " key={index}>
+                            <div className="row ">
+                              <div className="col-1">
+                                <div className="wrapper-img">
+                                  <img
+                                    src={Images}
+                                    alt="img"
+                                    className="img-fluid  anime-img"
+                                  />
+                                </div>
                               </div>
-                            </div>
-                            <div className="col-6 mx-4">
-                              <div className="wrap-my-content">
-                                <div className="my-content">
-                                  <div className="anime-header">
-                                    <p className="anime-title">{el.name}</p>
-                                  </div>
-                                  <div className="anime-btns">
-                                    {el.nicknames.map((item) => (
-                                      <p className="my-2 me-4 my-btn">{item}</p>
-                                    ))}
+                              <div className="col-6 mx-4">
+                                <div className="wrap-my-content">
+                                  <div className="my-content">
+                                    <div className="anime-header">
+                                      <p className="anime-title">{el.PFName}</p>
+                                    </div>
+                                    <div className="anime-btns">
+                                      <p className="my-2 me-4 my-btn">
+                                        {el.SkillDesc}
+                                      </p>
+                                    </div>
                                   </div>
                                 </div>
                               </div>
-                            </div>
-                            <div className="col-2">
-                              <div className="wrap-my-heart h-100 my-3">
-                                <p className="">
-                                  <BsFillHeartFill className="mx-2 text-danger" />
-                                  {el.favorites}
-                                </p>
+                              <div className="col-2">
+                                <div className="wrap-my-heart h-100 my-3">
+                                  <p className="">
+                                    <b>Value :</b>
+                                    {"$" + el.Value + "M"}
+                                  </p>
+                                </div>
                               </div>
-                            </div>
-                            <div className="col-2">
-                              <div className="my-arrow h-100">
-                                <div className="d-flex justify-content-end align-items-center h-100">
-                                  <img
-                                    src={arrowImage}
-                                    alt="arrow"
-                                    className="img-fluid my-arrow-img"
-                                  />
+                              <div className="col-2">
+                                <div className="my-arrow h-100">
+                                  <div className="d-flex justify-content-end align-items-center h-100">
+                                    <img
+                                      src={arrowImage}
+                                      alt="arrow"
+                                      className="img-fluid my-arrow-img"
+                                    />
+                                  </div>
                                 </div>
                               </div>
                             </div>
                           </div>
-                        </div>
-                      </>
-                    );
-                  })
-                ) : (
-                  <>
-                    <h2>No Results Found!</h2>
-                  </>
-                )}
+                        </>
+                      );
+                    })
+                  : this.state.searchArr.map((el, index) => {
+                      const Images = React.lazy(() =>
+                        import(`../../player-images/${el.Id}`)
+                      );
+                      return (
+                        <>
+                          <div className="card my-2 shadow " key={index}>
+                            <div className="row ">
+                              <div className="col-1">
+                                <div className="wrapper-img">
+                                  <img
+                                    src={Images}
+                                    alt="img"
+                                    className="img-fluid  anime-img"
+                                  />
+                                </div>
+                              </div>
+                              <div className="col-6 mx-4">
+                                <div className="wrap-my-content">
+                                  <div className="my-content">
+                                    <div className="anime-header">
+                                      <p className="anime-title">{el.PFName}</p>
+                                    </div>
+                                    <div className="anime-btns">
+                                      <p className="my-2 me-4 my-btn">
+                                        {el.SkillDesc}
+                                      </p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="col-2">
+                                <div className="wrap-my-heart h-100 my-3">
+                                  <p className="">
+                                    <b>Value :</b>
+                                    {"$" + el.Value + "M"}
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="col-2">
+                                <div className="my-arrow h-100">
+                                  <div className="d-flex justify-content-end align-items-center h-100">
+                                    <img
+                                      src={arrowImage}
+                                      alt="arrow"
+                                      className="img-fluid my-arrow-img"
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+                      );
+                    })}
               </div>
-            </div>
-            <div className="container d-flex justify-content-between my-3">
-              <button
-                disabled={this.state.page <= 1}
-                type="button"
-                className="btn btn-dark mr-4"
-                onClick={this.clickPrevButton}
-              >
-                &larr; Prev
-              </button>
-              <button
-                disabled={
-                  this.state.page + 1 > Math.ceil(this.state.animeLength / 15)
-                }
-                type="button"
-                className="btn btn-dark mr-4"
-                onClick={this.clickNextButton}
-              >
-                Next &rarr;
-              </button>
             </div>
           </div>
         </div>
